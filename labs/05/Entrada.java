07e03f409dd2ce4d7f6c4daba5c046fa2dde0cea
harysuarez <141367310+harysuarez@users.noreply.github.com>
2023-11-01 22:38:03 -0500

    Add files via upload

--- labs/05/Entrada.java
+++ labs/05/Entrada.java
@@ -25,6 +25,74 @@ public class Entrada {
             System.out.println("Error al leer el archivo de municipios: " + e.getMessage());
         }
     }
-}
     
-   
+   public int[] subsidiosPorEstadoTodos(){
+        int subsidiosDia[] = new int [8];
+        int i=0;
+        while (i<subsidios.size()){
+            Subsidio temp = subsidios.get(i);
+            
+            switch (temp.estado) {
+                case "Asignados": subsidiosDia[0] += 1;
+                                break;
+                case "Renuncias al subsidio":   subsidiosDia[1] += 1;
+                                break;
+                case "Apto con subsidio vencido":  subsidiosDia[2] += 1;
+                                break;
+                case "Asignado con aplicacion en Vivienda Gratuita":   subsidiosDia[3] += 1;
+                                    break;
+                case "Restitucion SFV por Indemnizacion":  subsidiosDia[4] += 1;
+                                break;
+                case "Restitucion de subsidios no aplicados": subsidiosDia[5] += 1;
+                                break;
+                case "Renuncia a la postulacion":  subsidiosDia[6] += 1;
+                                break;
+                default:    subsidiosDia[7]+=1;
+                            break;
+            }
+            i++;
+        }
+        return subsidiosDia;
+    }
+    
+    public int[] subsidiosPorEstado2014() {
+        int subsidiosDia[] = new int[8];
+        int i = 0;
+        while (i < subsidios.size()) {
+            Subsidio temp = subsidios.get(i);
+
+            if(temp.anio>2013){
+                switch (temp.estado) {
+                    case "Asignados":
+                        subsidiosDia[0] += 1;
+                        break;
+                    case "Renuncias al subsidio":
+                        subsidiosDia[1] += 1;
+                        break;
+                    case "Apto con subsidio vencido":
+                        subsidiosDia[2] += 1;
+                        break;
+                    case "Asignado con aplicacion en Vivienda Gratuita":
+                        subsidiosDia[3] += 1;
+                        break;
+                    case "Restitucion SFV por Indemnizacion":
+                        subsidiosDia[4] += 1;
+                        break;
+                    case "Restitucion de subsidios no aplicados":
+                        subsidiosDia[5] += 1;
+                        break;
+                    case "Renuncia a la postulacion":
+                        subsidiosDia[6] += 1;
+                        break;
+                    default:
+                        subsidiosDia[7]+=1;
+                        break;
+                }
+            }
+            i++;
+        }
+        return subsidiosDia;
+    }
+    
+    
+}
