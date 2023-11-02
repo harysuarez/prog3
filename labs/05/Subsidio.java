120a48676bd6372ca76800c18fd142aa3e451ff4
harysuarez <141367310+harysuarez@users.noreply.github.com>
2023-11-01 21:57:21 -0500

    Add files via upload

---
+++ labs/05/Subsidio.java
@@ -0,0 +1,13 @@
+//Municipio,Código Divipola Municipio,Programa,Año de Asignación,Estado de Postulación,Hogares,Valor Asignado
+public class Subsidio {
+    String programa;
+    int anio;
+    String estado;
+
+    public Subsidio(String programa, int anio, String estado) {
+        this.programa = programa;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+}
