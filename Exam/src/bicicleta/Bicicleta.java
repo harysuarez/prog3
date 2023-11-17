6df0d48f104ae52862671c9748504f88ad796bf3
harysuarez <141367310+harysuarez@users.noreply.github.com>
2023-11-16 19:42:17 -0500

    Add files via upload

--- Exam/src/bicicleta/Bicicleta.java
+++ Exam/src/bicicleta/Bicicleta.java
@@ -1,33 +1,21 @@
-
-package bicicleta;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class Bicicleta {
-private static int contador = 0;
-private static int tiempo;
-
-public static void main(String[] args) {
- contador = 3;
- 
- }
-
-public class Temporizador0 {
- Timer timer;
- public Temporizador0(int segundos) {
- timer = new Timer();
- timer.schedule(new Recordatorio(),segundos*1000);//Programa la tarea
- }
- class Recordatorio extends TimerTask {
- public void run() {
- System.out.println("Tiene un recordatorio!");
- timer.cancel(); //Termina el hilo del timer
- }
- }
- public static void main(String args[]) {
- System.out.println("Inicio del programa");
- new Temporizador0(5);
- }
-}
-
-}
+//package bicicleta;
+//
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+// */
+//
+///**
+// *
+// * @author harym
+// */
+//public class Bicicleta {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
+//    
+//}
